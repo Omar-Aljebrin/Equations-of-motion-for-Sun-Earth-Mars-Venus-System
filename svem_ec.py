3020e4e9bf8e4c4e982c69d040e7f9166a3d4a9e
Omar Aljebrin <161967585+Omar-Aljebrin@users.noreply.github.com>
2025-05-30 13:12:02 -0400

    Update svem_ec.py

--- svem_ec.py
+++ svem_ec.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#i was here again
 
 # -----------------------------------------------------------------------------
 # END GLOBAL VAR - GLOBAL MODS
